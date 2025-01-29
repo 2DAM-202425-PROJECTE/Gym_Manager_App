0c3c68de8b69c4739a4ea59f960ffbc97778af8d
pietroscorza <pietroscorza@iesebre.com>
2025-01-29 20:36:07 +0100

    ruta per al registre

--- GymManager/routes/api.php
+++ GymManager/routes/api.php
@@ -12,6 +12,7 @@ Route::get('/user', function (Request $request) {
 */
 
 Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
 
 Route::apiResource('users', UserController::class);
 
