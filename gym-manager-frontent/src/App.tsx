29f9b14e8517ff105894889fdb5d5469dcfd7284
pietroscorza <pietroscorza@iesebre.com>
2025-01-31 20:12:16 +0100

    traducciones en login i registre

--- gym-manager-frontent/src/App.tsx
+++ gym-manager-frontent/src/App.tsx
@@ -9,11 +9,14 @@ import Profile from './pages/Profile';
 import Pago from './pages/Pago';
 import { UserProvider } from './context/userContext';
 import DashboardAdmin from './pages/admin/DashboardAdmin';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n'; 
 
 function App() {
 
   return (
-    <UserProvider>
+   <I18nextProvider i18n={i18n}>
+     <UserProvider>
       <div>
        <Routes>
           <Route index element={<Home />}/>
@@ -26,6 +29,7 @@ function App() {
       </Routes>
     </div>
     </UserProvider>
+   </I18nextProvider>
     )
 }
 
