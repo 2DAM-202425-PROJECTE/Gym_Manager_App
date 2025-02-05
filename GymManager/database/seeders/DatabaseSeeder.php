145f5860124583621d1cb9914696648d1b40852f
fheius1 <fheius@iesebre.com>
2025-02-05 20:39:06 +0100

    Seeder

--- GymManager/database/seeders/DatabaseSeeder.php
+++ GymManager/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,7 +15,39 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::truncate();
         User::factory(10)->create();
 
+        // Crear un usuario normal
+        User::factory()->create([
+            'name' => 'Miquel Agudo',
+            'email' => 'nomembresia@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => 'client',
+        ]);
+
+        // Crear un usuario con membresía
+        $userWithMembresia = User::factory()->create([
+            'name' => 'Oscar Fumador',
+            'email' => 'membresia@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => 'client',
+        ]);
+
+        // Asignar membresía al usuario
+        $userWithMembresia->membresia()->create([
+            'user_id' => $userWithMembresia->id,
+            'fecha_fin' => now()->addYear(),
+            'qr_data' =>  Str::uuid()->toString(),
+        ]);
+
+
+        // Crear un administrador
+        User::factory()->create([
+            'name' => 'Administrador',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+        ]);
     }
 }
